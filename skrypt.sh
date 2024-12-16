34c8c232892126a310f628c05ddcf0a6f3583eea
Marcin Jukiewicz <marcinjukiewicz@gmail.com>
2024-12-16 13:47:26 +0100

    Update skrypt.sh

--- skrypt.sh
+++ skrypt.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #
-#SBATCH --job-name=3des1val
+#SBATCH --job-name=zad_konwolucje
 #SBATCH --output=moje_zadanie.out
 #SBATCH --error=moje_zadanie.err
 #SBATCH --partition=dziobas
 #SBATCH --nodes=1
 #SBATCH --tasks-per-node=40
-#SBATCH --time=720:00:00
+#SBATCH --time=20:00:00
 
 python sprawdzenie.py
